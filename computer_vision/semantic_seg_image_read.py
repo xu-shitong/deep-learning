6ec9b0c8cbe53346f3448ad735139cd6ec45915e
shitong xu <tonyshengxue@outlook.com>
2021-07-28 07:08:18 +0100

    xst: 07:07 07/28: final update deep learning notes

--- computer_vision/semantic_seg_image_read.py
+++ computer_vision/semantic_seg_image_read.py
@@ -13,11 +13,11 @@ train_feature, train_labels = d2lzh.read_voc_images(voc_dir)
 x = 200
 y = 196
 img = train_labels[0]
-# fig = d2lzh.plt.imshow(img.asnumpy())
+fig = d2lzh.plt.imshow(img.asnumpy())
 
-# # mark the pixel being examined
-# mark = d2lzh.plt.Rectangle(xy=(x, y), width=2, height=2, edgecolor='blue', linewidth=2)
-# fig.axes.add_patch(mark)
+# mark the pixel being examined
+mark = d2lzh.plt.Rectangle(xy=(x, y), width=2, height=2, edgecolor='blue', linewidth=2)
+fig.axes.add_patch(mark)
 
 # # get pixel info and lookup label
 # img_matrix = img.astype('int32')
@@ -26,11 +26,11 @@ img = train_labels[0]
 # print(d2lzh.VOC_CLASSES[int(colormap2label[pixel_index.asscalar()].asscalar())])
 
 # preprocess one image, cut in different sizes
-imgs = [[], []]
-for i in range(0,5):
-  feature, label = d2lzh.voc_rand_crop(train_feature[0], train_labels[0], 225, 225)
-  imgs[0].append(feature)
-  imgs[1].append(label)
-d2lzh.show_images(imgs[0] + imgs[1], 1, 5)
+# imgs = [[], []]
+# for i in range(0,5):
+#   feature, label = d2lzh.voc_rand_crop(train_feature[0], train_labels[0], 225, 225)
+#   imgs[0].append(feature)
+#   imgs[1].append(label)
+# d2lzh.show_images(imgs[0] + imgs[1], 1, 5)
 
 d2lzh.plt.show()
