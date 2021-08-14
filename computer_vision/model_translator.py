ea558eeab7f51b2e6a904291f7e25fe99079248f
shitong xu <tonyshengxue@outlook.com>
2021-08-14 16:18:49 +0100

    xst: 16:10 14/08: add program for experimenting for style trasnfer

--- computer_vision/model_translator.py
+++ computer_vision/model_translator.py
@@ -1,15 +1,29 @@
 # This program is used for transfer model imported from mxnet.model_zoo to numpy
 # Note that there are alternative routes to translate the network
 
-from mxnet.gluon import model_zoo
+from mxnet.gluon import model_zoo, nn as m_nn
 import numpy as np
 
+import torchvision.models as models
+from torch import nn as t_nn
+import torch
+
 pretrained_net = model_zoo.vision.vgg19(pretrained=True)
-output = []
+torch_vgg19 = models.vgg19(pretrained=True)
+
+net = t_nn.Sequential()
+
 for i in range(29):
-  param = pretrained_net.features[i].weight.data()
-  output.append(
-    param.asnumpy()
-  )
-  
-np.savetxt('param.txt', output)
+  mxnet_layer = pretrained_net.features[i]
+  torch_layer = torch_vgg19.features[i]
+  if isinstance(mxnet_layer, m_nn.Conv2D):
+    weight = mxnet_layer.weight.data().asnumpy()
+    bias = mxnet_layer.bias.data().asnumpy()
+    with torch.no_grad():
+      torch_layer.weight.copy_(torch.tensor(weight))
+      torch_layer.bias.copy_(torch.tensor(bias))
+  net.add_module(f"layer {i}", torch_layer)
+  print(f"finish upto layer: {i}")
+
+print("finish transfering, writing to file param.log")
+torch.save(net, 'param.log')
